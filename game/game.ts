db59ee75f9c6e49a03886d1e56587702a52aa645
Jacob Hixon <jacobhixon47@gmail.com>
2017-05-17 01:24:28 -0700

    commit animations etc and add tilemap json..

--- game/game.ts
+++ game/game.ts
@@ -2,13 +2,11 @@
 /// <reference path="resources.ts" />
 /// <reference path="config.ts" />
 /// <reference path="player.ts" />
-
 var game = new ex.Engine({
     width: undefined,
     height: undefined,
     displayMode: ex.DisplayMode.Container
 });
-
 // create an asset loader
 var resources = {
   texturePlayerDown: Resources.texturePlayerWalkDown,
@@ -16,7 +14,6 @@ var resources = {
   texturePlayerLeft: Resources.texturePlayerWalkLeft,
   texturePlayerRight: Resources.texturePlayerWalkRight
 }
-
 // queue resources for loading
 var loader = new ex.Loader();
 var player = new Player();
@@ -24,7 +21,6 @@ var player = new Player();
 for (var r in resources) {
   loader.addResource(resources[r]);
 }
-
 game.start(loader).then(function() {
   // spritePlayer.scale = new ex.Vector(2, 2);
   game.add(player);
